318eba1b293e9aa8b1920799ae606e0dde1095bf
Gokul Girish <gokulgirish@Gokuls-MacBook-Pro.local>
2024-09-13 21:11:11 +0530

    font issue

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,15 +2,23 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-        extend: {
+    extend: {
       keyframes: {
-        'infinite-scroll': {
-          '0%': { transform: 'translateX(0)' },
-          '100%': { transform: 'translateX(-100%)' },
+        "infinite-scroll": {
+          "0%": { transform: "translateX(0)" },
+          "100%": { transform: "translateX(-100%)" },
         },
       },
       animation: {
-        'infinite-scroll': 'infinite-scroll 20s linear infinite',
+        "infinite-scroll": "infinite-scroll 20s linear infinite",
+      },
+      fontSize: {
+        sm: ["0.875rem", { lineHeight: "1.25rem" }],
+        base: ["1rem", { lineHeight: "1.5rem" }],
+      },
+      padding: {
+        sm: "0.5rem",
+        md: "1rem",
       },
     },
   },
